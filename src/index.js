360e560c1e23c96828f9bba4d7993f3cd0d2d5aa
Arkhipenka <europeoplebelarus@gmail.com>
2018-02-25 22:43:01 +0300

    100%

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var i = 5;
+  var result = 0;
+  while(number > i) {
+    result += Math.floor(number / i);
+    i *= 5;
+  }
+  return result;
 }
